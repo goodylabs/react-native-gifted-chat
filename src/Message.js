4da014316022fbc67a64212b9d73ce63a3b5f14f
Radoslaw Tarnas <radoslaw.tarnas@gmail.com>
2017-02-24 16:34:17 +0100

    custom Message container style

--- src/Message.js
+++ src/Message.js
@@ -61,7 +61,7 @@ export default class Message extends React.Component {
 
   render() {
     return (
-      <View>
+       <View style={this.props.messageContainerStyle}>
         {this.renderDay()}
         <View style={[styles[this.props.position].container, {
           marginBottom: isSameUser(this.props.currentMessage, this.props.nextMessage) ? 2 : 10,
